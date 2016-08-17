c675edcda72e5e3c2c5d2fae39508159ded62aa2
abelbour <abelbour2k@yahoo.com>
2016-08-17 20:39:25 -0300

    link para descargar scripts de emision

---
+++ tools/parque-finde.bat
@@ -0,0 +1,4 @@
+FOR /L %%A IN (1,1,200) DO (
+  ffmpeg -f dshow -thread_queue_size 4096 -rtbufsize 702000k -i audio="@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\wave_{3830DE51-E911-436C-9A50-EA2426495EE2}" -thread_queue_size 4096 -i "rtsp://192.168.1.114:554/user=admin&password=&channel=1&stream=0.sdp" -ac 1 -filter:a alimiter=limit=.3 -codec:a aac -cutoff 8000 -b:a 32k -vcodec copy -aspect 16:9 -f flv "rtmp://publish.dailymotion.com/publish-dm/x4p2j4r?auth=0I13_0a716891ec3087be3a891108b8c9a75d837b6ecc"
+
+)
