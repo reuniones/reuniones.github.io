a9157c74a5c707ddc900ffa6c82c81864f5a8eb6
abelbour <abelbour2k@yahoo.com>
2016-08-17 10:24:59 -0300

    primera version x congregacion, norte

---
+++ norte/config.js
@@ -0,0 +1,34 @@
+﻿(function (window) {
+    window.__env = window.__env || {};
+    window.__env.videoIdSemana = 'x4ozkh3';
+    window.__env.videoIdFinde = 'x4ozlcm';
+    window.__env.videoUrlDigestor1 = '//www.dailymotion.com/embed/video/';
+    window.__env.videoUrlDigestor2 = '?api=postMessage&id=player&syndication=lr:175159&autoplay=1&mute=0&info=0&logo=0&related=0&social=0&controls=1&quality=auto&html=1&chromeless=0&theme=dark';
+    window.__env.videoUrlDigestor2Flash = '?api=postMessage&id=player&syndication=lr:175159&autoplay=1&mute=0&info=0&logo=0&related=0&social=0&controls=1&quality=auto&html=0&chromeless=0&theme=dark';
+    window.__env.videoUrlStream = '//www.dailymotion.com/embed/video/';
+    window.__env.nombreCongregacion = 'Co. Norte';
+    window.__env.programaIdSheets = '1BzuB98iqVhP6h2hr6RJELaRFOc9O2A5bj74mvQyapSQ';
+    window.__env.programaUrl1 = '//docs.google.com/spreadsheets/d/';
+    window.__env.programaUrl2 = '/pubhtml'; //?widget=true&headers=false&chrome=false';
+    window.__env.programaUrlDescargaPdf = '/export?format=pdf';
+    window.__env.salidasIdSheets = '1JuiB_XVz-jO_0Yrzu82pF2d3cbWkDhBwhcp5wF2lOrA';
+    window.__env.salidasUrl1 = '//docs.google.com/spreadsheets/d/';
+    window.__env.salidasUrl2 = '/pubhtml?gid=0&single=true&widget=false&headers=false&chrome=false';
+    //switches para activar los menus
+    window.__env.mnuPredicacion = true;
+    window.__env.mnuSalidas = true;
+    window.__env.mnuTerritorios = true;
+    window.__env.mnuInforme = true;
+    window.__env.mnuReuniones = true;
+    window.__env.mnuPrograma = true;
+    window.__env.mnuEmision = true;
+    window.__env.emisionAsistencia = true;
+    window.__env.paginaInicio = '/reuniones/emision.html';
+    // API url
+   
+    // Base url
+    window.__env.baseUrl = '/';
+    // Whether or not to enable debug mode
+    // Setting this to false will disable console output
+    window.__env.enableDebug = true;
+}(this));
