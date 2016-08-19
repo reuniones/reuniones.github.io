57ab69fee5eff170ba7989aff1e368e4761fa15f
abelbour <abelbour2k@yahoo.com>
2016-08-19 10:38:37 -0300

    agregada pagina de testing

---
+++ test/config.js
@@ -0,0 +1,34 @@
+﻿(function (window) {
+    window.__env = window.__env || {};
+    window.__env.videoIdSemana = 'x4o08h1';
+    window.__env.videoIdFinde = 'x4o08h1';
+    window.__env.videoUrlDigestor1 = '//www.dailymotion.com/embed/video/';
+    window.__env.videoUrlDigestor2 = '?api=postMessage&id=player&syndication=lr:175159&autoplay=1&mute=0&info=0&logo=0&related=0&social=0&controls=1&quality=auto&html=1&chromeless=0&theme=dark';
+    window.__env.videoUrlDigestor2Flash = '?api=postMessage&id=player&syndication=lr:175159&autoplay=1&mute=0&info=0&logo=0&related=0&social=0&controls=1&quality=auto&html=0&chromeless=0&theme=dark';
+    window.__env.videoUrlStream = '//www.dailymotion.com/embed/video/';
+    window.__env.nombreCongregacion = 'Test';
+    window.__env.programaIdSheets = '1BzuB98iqVhP6h2hr6RJELaRFOc9O2A5bj74mvQyapSQ';
+    window.__env.programaUrl1 = '//docs.google.com/spreadsheets/d/';
+    window.__env.programaUrl2 = '/pubhtml'; //?widget=true&headers=false&chrome=false';
+    window.__env.programaUrlDescargaPdf = '/export?format=pdf';
+    window.__env.salidasIdSheets = '1JuiB_XVz-jO_0Yrzu82pF2d3cbWkDhBwhcp5wF2lOrA';
+    window.__env.salidasUrl1 = '//docs.google.com/spreadsheets/d/';
+    window.__env.salidasUrl2 = '/pubhtml?gid=0&single=true&widget=false&headers=false&chrome=false';
+    //switches para activar los menus
+    window.__env.mnuPredicacion = false;
+    window.__env.mnuSalidas = false;
+    window.__env.mnuTerritorios = false;
+    window.__env.mnuInforme = false;
+    window.__env.mnuReuniones = false;
+    window.__env.mnuPrograma = false;
+    window.__env.mnuEmision = false;
+    window.__env.emisionAsistencia = false;
+    window.__env.paginaInicio = '/reuniones/emision.html';
+    // API url
+   
+    // Base url
+    window.__env.baseUrl = '/';
+    // Whether or not to enable debug mode
+    // Setting this to false will disable console output
+    window.__env.enableDebug = true;
+}(this));
