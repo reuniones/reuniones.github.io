e540e925de9649acbba33e3f6ea086f1484dc2c8
Abel Bourband <abelbour@australsp.com>
2026-03-09 17:24:54 -0300

    Configure master deployment and sync files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Asegura que las rutas funcionen en GitHub Pages si se despliega en una subcarpeta
+  base: '/reuniones/', // Cambiado de './' a '/reuniones/' para coincidir con la subcarpeta en GitHub Pages
 })
